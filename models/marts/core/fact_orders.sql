7cd235decae012d0ff6b9a75119396d1f06df005
dzickuhr <douglas.zickuhr@optum.com>
2021-10-21 22:24:29 +0100

    Refactoring fact_orders

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -1,10 +1,20 @@
-with orders_payments as 
-(SELECT 
-order_id,
-customer_id,
-payment_amount AS amount
-FROM 
-{{ref("stg_orders")}}
-JOIN {{ref("stg_payments")}} USING (order_id))
+with payments as (
+    SELECT
+    order_id,
+    SUM(payment_amount) as payment_amount
+    FROM
+    {{ ref("stg_payments") }}
+    WHERE payment_status = 'success'
+    GROUP BY order_id
+),
+orders_payments as (
+    SELECT 
+    order_id,
+    customer_id,
+    coalesce(payment_amount, 0) AS amount
+    FROM 
+    {{ ref("stg_orders") }}
+    LEFT JOIN payments USING (order_id)
+)
 
-select * from orders_payments
+SELECT * FROM orders_payments
