d175e3232ad12e373d17c21aa9757d5ca5e89102
dzickuhr <douglas.zickuhr@optum.com>
2021-10-24 23:52:51 +0100

    Adding stg_dates model

--- models/staging/jaffle_shop/stg_dates.sql
+++ models/staging/jaffle_shop/stg_dates.sql
@@ -4,4 +4,9 @@
     )
 }}
 
-{{ dbt_utils.date_spine("day", "cast ('2020-01-01' as date)", "cast ('2020-12-31' as date)") }}
+{{ dbt_utils.date_spine(
+    "day", 
+    "cast ('2020-01-01' as date)", 
+    "cast ('2020-12-31' as date)"
+    ) 
+}}
